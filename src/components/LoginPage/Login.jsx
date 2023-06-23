4d5cb7fc6ddc26da3997e0fa1e449dd87a4d2428
Shachi <jn.shachi@gmail.com>
2023-06-23 21:19:57 +0530

    google done

--- src/components/LoginPage/Login.jsx
+++ src/components/LoginPage/Login.jsx
@@ -1,16 +1,32 @@
 import '../../styles/login.scss'
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
 
 const Login = () => {
   return (
     <div className="container">
+
       <div className="left-container">
         <span>Board.</span>
       </div>
+
       <div className="right-container">
-        <div className="login-form">
-          <b>Sign In</b>
-          <br/>
-          <span>Sign in to your account </span>
+        <div className="login-text">
+          <b>Sign In</b><br/>
+          <span> Use Google for Login</span>
+        </div>
+        <div className='google-button'>
+          <GoogleOAuthProvider clientId="1037231595534-m3f4un5nqhl4rcms6njbo0bdn78kfvip.apps.googleusercontent.com">
+              <GoogleLogin
+                onSuccess={credentialResponse => {
+                  var decoded = jwt_decode(credentialResponse.credential);
+                  console.log(decoded);
+                }}
+                onError={() => {
+                  console.log('Login Failed');
+                }}
+              />
+          </GoogleOAuthProvider>
         </div>
       </div>
     </div>
